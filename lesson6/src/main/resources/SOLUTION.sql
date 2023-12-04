2331508c9501c0540ad64e89482c9bda3f8e55d1
Armen <mirzoyana805@gmail.com>
2023-12-04 22:23:45 +0400

    Commit for lesson 6 part 1

---
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -0,0 +1,23 @@
+select p.*
+from PAYMENT p
+join PUBLIC.PAYMENTTYPE P2 on P2.ID = p.TYPE_ID
+where p2.NAME = 'MONTHLY';
+
+select m.*
+from MARK m
+join PUBLIC.SUBJECT S on S.ID = m.SUBJECT_ID
+where s.NAME = 'Art';
+
+select s.*
+from STUDENT s
+ join PAYMENT p
+    on s.ID = p.STUDENT_ID
+ join PAYMENTTYPE pt
+    on p.TYPE_ID = pt.ID
+where pt.NAME = 'WEAKLY';
+
+select s.*
+from STUDENT s
+ join PUBLIC.MARK M on s.ID = M.STUDENT_ID
+ join SUBJECT su on M.SUBJECT_ID = su.ID
+where su.NAME = 'Math';
