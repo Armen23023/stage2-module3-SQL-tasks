e24d05ed81f93127efe75b2eb9b6e10205ff9464
Armen <mirzoyana805@gmail.com>
2023-12-09 14:38:57 +0400

    Commit for lesson 9 part 1

---
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -0,0 +1,25 @@
+Select s.* from STUDENT s
+ join (
+    SELECT STUDENT_ID, avg(MARK)
+     from MARK
+     group by STUDENT_ID
+     having avg(MARK) > 8
+ ) m on s.ID = m.STUDENT_ID;
+
+
+SELECT s.ID,s.NAME from STUDENT s
+ join (
+     SELECT STUDENT_ID, MARK
+      from MARK
+      group by STUDENT_ID
+      having min(MARK) > 7
+ ) m on s.ID = m.STUDENT_ID;
+
+
+SELECT  s.ID, s.NAME from STUDENT s
+ join (
+     SELECT STUDENT_ID, PAYMENT_DATE
+      from  PAYMENT
+       group by STUDENT_ID
+        having  count(PAYMENT_DATE) > 2
+) p on s.ID = p.STUDENT_ID;
