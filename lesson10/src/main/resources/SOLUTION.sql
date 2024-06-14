d437aaa0987aead7cc6fecb52475d58f501b7857
Armen <mirzoyana805@gmail.com>
2024-06-14 15:55:01 +0400

    Commit for lesson 10 part 1

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -6,3 +6,9 @@ SELECT s.*
      GROUP BY subject_id
      HAVING AVG(mark) > (SELECT AVG(mark) FROM MARK)
  ) m ON s.id = m.subject_id;
+
+
+SELECT s.*
+FROM student s
+         JOIN payment p ON s.id = p.student_id
+WHERE p.amount < (SELECT AVG(amount) FROM payment);
